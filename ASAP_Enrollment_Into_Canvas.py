a51c1a968459d8d83aa12bebdce44d77fc887c2d
Eric Dannewitz <ericdannewitz@me.com>
2021-12-20 23:16:27 -0800

    Update ASAP_Enrollment_Into_Canvas.py

--- ASAP_Enrollment_Into_Canvas.py
+++ ASAP_Enrollment_Into_Canvas.py
@@ -50,8 +50,8 @@ if configs['SendCOVIDLetters'] == "True":
     SentCOVIDLetters = pd.read_csv(Path(configs['COVIDLetterPath']+configs['SentCOVIDLettersCSV']))
 #
 #
+globals msgbody,skippedbody,dmsgbody
 def Set_globals():
-    global msgbody,skippedbody,dmsgbody
     msgbody = ''
     skippedbody = ''
     dmsgbody = ''
@@ -312,7 +312,8 @@ elif r.status_code == 200:
                         for login in userlogins:
                             if login.unique_id == olduseremail:
                                 foundanotherlogin=True
-                                logging.info('Found ASAPs current default email as a login')
+                                print('Found ASAPs current default email ' emailaddr.lower() + 'as a login')
+                                logging.info('Found ASAPs current default email ' emailaddr.lower() + 'as a login')
                         logging.info('Seems that we have someone ' + str(newenrolls['CustomerID'][i]) + ' who changed their ASAP email, so lets change it in Canvas and add the old one as a Login for them\n')
                         if configs['Debug'] == "True":
                             dmsgbody += 'CustomerID ' + str(newenrolls['CustomerID'][i]) + ' is associated with a different email\n'
