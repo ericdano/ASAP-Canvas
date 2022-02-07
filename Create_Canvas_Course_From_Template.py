faa616f4d18407725ce69c1f98464fd478effddb
Eric Dannewitz <ericdannewitz@me.com>
2022-02-07 15:01:53 -0800

    New Stuff

--- Create_Canvas_Course_From_Template.py
+++ Create_Canvas_Course_From_Template.py
@@ -7,24 +7,15 @@ from email.message import EmailMessage
 # Useage C:\python Canvas_Course_Duplication.py nameofcsv.csv
 #
 
+# This takes a CSV file of new course codes and creates generic templates for enrollment purposes
+# CSV file needs CourseName, NewSIS_ID, Email, Template, SubAccount
 
-# NOT WORKING!!!!!!! NOT WORKING!!!!!!! NOT WORKING!!!!!!!
-
-# This program reads a CSV file with the fields of
-# NewSIS_ID, CurrentSIS_ID, CourseName
-# It will create a new course with the SIS_ID from the CSV or use a course that already has the SIS_ID
-# and copy the CurrentSIS_ID course contents to the new course
-# and enroll the CurrentSIS_ID course teacher in the new course as the teacher
-# version .01
-# Created to help more easily roll over classes to a new term for Acalanes Adult Ed
-#
-# New classes are created using a Term defined here set to what it is in your Canvas Instance
-if len(sys.argv) < 4:
-    print('Script needs three arguments: python name.csv "term" "teacher@mail.com')
+if len(sys.argv) < 3:
+    print('Script needs two arguments: python name.csv term (term needs to be in single quotes)')
     exit(0)
-newcoursecode = sys.argv[1]
+csvfilename = sys.argv[1]
 CurrentCanvasTerm = sys.argv[2]
-teacheremail = sys.argv[3]
+#teacheremail = sys.argv[3]
 #load configs
 home = Path.home() / ".ASAPCanvas" / "ASAPCanvas.json"
 confighome = Path.home() / ".ASAPCanvas" / "ASAPCanvas.json"
@@ -44,6 +35,9 @@ dmsg['Subject'] = str("Debug - " + configs['SMTPStatusMessage'] + " " + datetime
 dmsg['From'] = configs['SMTPAddressFrom']
 dmsg['To'] = configs['DebugEmailAddr']
 dmsgbody = ''
+#read the csv file
+asapcoursestocopy = pd.read_csv(csvfilename, dtype=str)
+print('Loaded CSV....')
 #Function to Copy Course
 def copy_to_new_course():
     print('Copying Course....')
@@ -52,14 +46,12 @@ def copy_to_new_course():
     # Get new course
     new_course = canvas.get_course(asapcoursestocopy['NewSIS_ID'][i], use_sis_id=True)
     # Get old course
-    old_course = canvas.get_course(asapcoursestocopy['CurrentSIS_ID'][i], use_sis_id=True)
+    old_course = canvas.get_course(asapcoursestocopy['Template'][i], use_sis_id=True)
     # Start migration
     new_course.create_content_migration('course_copy_importer', settings={'source_course_id': old_course.id})
     # Get the Canvas teacher(s) and enroll them into the copied course
-    users = old_course.get_users(enrollment_type=['teacher'])
-    for user in users:
-        #canvas_user = canvas.get_user("Email", id_type="sis_login_id")
-        new_course.enroll_user(user.id, "TeacherEnrollment", enrollment={"enrollment_state": "active"})
+    canvas_user = canvas.get_user(asapcoursestocopy['Email'][i], id_type="sis_login_id")
+    new_course.enroll_user(canvas_user.id, "TeacherEnrollment", enrollment={"enrollment_state": "active"})
 
 logging.info('Connecting to Canvas')
 if configs['Debug'] == "True":
@@ -69,6 +61,7 @@ account = canvas.get_account(1)
 #get get_enrollment_terms
 terms = account.get_enrollment_terms()
 term_id = 0
+print('Looking for ' + CurrentCanvasTerm)
 for term in terms:
     if term.name == CurrentCanvasTerm:
         logging.info('Found Term ID')
@@ -81,29 +74,31 @@ if term_id == 0:
     dmsgbody=dmsgbody+'Did not find Term in Canvas. Aborted program.'
 else:
     logging.info('Copying Course')
-    try:
-        asapclass = canvas.get_course(asapcoursestocopy['NewSIS_ID'][i],use_sis_id=True)
-        logging.info('Course already in Canvas.....now copying......')
-        if configs['Debug'] == "True":
-            dmsgbody = dmsgbody + asapcoursestocopy['NewSIS_ID'][i] + ' is in Canvas\n'
-        copy_to_new_course()
-    except CanvasException as e:
-        if str(e) == "Not Found":
-            #course does not exist, create it
-            logging.info('Course ' + asapcoursestocopy['NewSIS_ID'][i] + ' is not in Canvas')
-            print('Course not in Canvas, creating')
-            dmsgbody = dmsgbody + asapcoursestocopy['NewSIS_ID'][i] + ' is NOT in Canvas\n'
-            old_course1 = canvas.get_course(asapcoursestocopy['CurrentSIS_ID'][i],use_sis_id=True)
-            subaccount = canvas.get_account(old_course1.account_id)
-            newCourse = subaccount.create_course(
-                                course={'name': asapcoursestocopy['CourseName'][i],
-                                'course_code': newcoursecode,
-                                'sis_course_id': newcoursecode,
-                                'term_id': term_id}
-                                )
-            print(newCourse)
-            dmsgbody = dmsgbody + 'created course ' + newcoursecode + ' in Canvas\n'
-            copy_to_new_course()
+    for i in asapcoursestocopy.index:
+        try:
+            asapclass = canvas.get_course(asapcoursestocopy['NewSIS_ID'][i],use_sis_id=True)
+            print('Course ' + str(asapcoursestocopy['NewSIS_ID'][i]) + ' is already in Canvas!!')
+#            logging.info('Course already in Canvas.....now copying......')
+#            if configs['Debug'] == "True":
+#               dmsgbody = dmsgbody + asapcoursestocopy['NewSIS_ID'][i] + ' is in Canvas\n'
+#            copy_to_new_course()
+        except CanvasException as e:
+            if str(e) == "Not Found":
+                #course does not exist, create it
+                logging.info('Course ' + asapcoursestocopy['NewSIS_ID'][i] + ' is not in Canvas')
+                print('Course not in Canvas, creating')
+                dmsgbody = dmsgbody + asapcoursestocopy['NewSIS_ID'][i] + ' is NOT in Canvas\n'
+                old_course1 = canvas.get_course(asapcoursestocopy['Template'][i],use_sis_id=True)
+                subaccount = canvas.get_account(asapcoursestocopy['SubAccount'][i])
+                newCourse = subaccount.create_course(
+                                    course={'name': asapcoursestocopy['CourseName'][i],
+                                    'course_code': asapcoursestocopy['NewSIS_ID'][i],
+                                    'sis_course_id': asapcoursestocopy['NewSIS_ID'][i],
+                                    'term_id': term_id}
+                                    )
+                print(newCourse)
+                dmsgbody = dmsgbody + 'created course ' + asapcoursestocopy['NewSIS_ID'][i] + ' in Canvas\n'
+                copy_to_new_course()
 s = smtplib.SMTP(configs['SMTPServerAddress'])
 if configs['Debug'] == "True":
     print("All done!")
