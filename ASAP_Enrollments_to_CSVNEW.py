1634f23d6135755f8465911a2add5c7ae2134620
Eric Dannewitz <ericdannewitz@me.com>
2022-04-06 11:12:03 -0700

    Update ASAP_Enrollments_to_CSVNEW.py

--- ASAP_Enrollments_to_CSVNEW.py
+++ ASAP_Enrollments_to_CSVNEW.py
@@ -11,9 +11,17 @@ except EnvironmentError:
     print('Config file not found. Check to make sure there is a .ASAPCanvas folder in your home directory with a ASAPCanvas.json file in that folder.')
     raise
 # Logging
-logfilename = Path.home() / ".ASAPCanvas" / configs['logfilename']
-logging.basicConfig(filename=str(logfilename), level=logging.INFO)
-logging.info('Loaded config file and logfile started')
+if configs['logserveraddress'] is None:
+    logfilename = Path.home() / ".ASAPCanvas" / configs['logfilename']
+    thelogger = logging.getLogger('MyLogger')
+    thelogger.basicConfig(filename=str(logfilename), level=logging.INFO)
+else:
+    thelogger = logging.getLogger('MyLogger')
+    thelogger.setlevel(logging.DEBUG)
+    handler = logging.handlers.SysLogHandler(address = configs['logserveraddress'],514)
+    thelogger = addHandler(handler)
+
+thelogger.info('Loaded config file and logfile started')
 csvfilename = sys.argv[1]
 #-----ASAP Info
 userid = configs['ASAPuserid']
@@ -22,17 +30,17 @@ password = configs['ASAPpassword']
 apikey = configs['ASAPAPIKey']
 url = configs['ASAPurl']
 headers = {'Authorization' : 'user='+userid+'&organizationId='+orgid+'&password='+password+'&apiKey='+apikey}
-logging.info('Getting ASAP Key')
+thelogger.info('Getting ASAP Key')
 r = requests.get(url,headers = headers)
 if r.status_code == 404:
-    logging.info('Failed to get ASAP Key')
+    thelogger.info('Failed to get ASAP Key')
 elif r.status_code == 200:
-    logging.info('Got ASAP Key')
+    thelogger.info('Got ASAP Key')
     accesstoken = r.json()
-    logging.info('Key is ' + accesstoken)
+    thelogger.info('Key is ' + accesstoken)
     url2 = configs['ASAPapiurl']
     header = {'asap_accesstoken' : accesstoken}
-    logging.info('Getting data from ASAP')
+    thelogger.info('Getting data from ASAP')
     r2 = requests.get(url2,headers = header)
     #print(r2)
     results = pd.concat([pd.json_normalize(r2.json()), pd.json_normalize(r2.json(),record_path="Students", max_level=2)], axis=1).drop(columns=['Students'])
